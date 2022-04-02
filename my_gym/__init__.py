a724d81a5531f5f1a4ad149198f6e0128eec36a8
osigaud <Olivier.Sigaud@upmc.fr>
2022-04-02 14:59:22 +0200

    my_gym OK

--- my_gym/__init__.py
+++ my_gym/__init__.py
@@ -1,5 +1,3 @@
-import gym
-
 from gym.envs.registration import register
 
 register(id="CartPoleContinuous-v0", entry_point="my_gym.envs:ContinuousCartPoleEnv", max_episode_steps=200)
--- tests/test_my_gym.py
+++ tests/test_my_gym.py
@@ -1,3 +1,4 @@
+import gym
 import my_gym
 
 
