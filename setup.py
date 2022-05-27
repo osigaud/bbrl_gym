33b7b4409ecc33c8b2a1fb6c1a6e18f1a374828f
osigaud <Olivier.Sigaud@upmc.fr>
2022-05-27 09:04:00 +0200

    working on installation

--- setup.py
+++ setup.py
@@ -3,16 +3,19 @@ from setuptools import find_packages, setup
 setup(
     name="my_gym",
     packages=[package for package in find_packages() if package.startswith("my_gym")],
+    url="https://github.com/osigaud/my_gym",
     version="0.0.1",
     install_requires=[
+        "git+https://github.com/osigaud/SimpleMazeMDP.git",
         "gym==0.21.0",
         "numpy>=1.19.1",
         "Box2D",
-        "git+https://github.com/osigaud/SimpleMazeMdp.git",
     ],
     tests_require=["pytest==4.4.1"],
     test_suite="tests",
-    description="Additional env gym library",
     author="Olivier Sigaud",
+    author_email="Olivier.Sigaud@isir.upmc.fr",
     license="MIT",
+    description="A set of additional gym environments",
+    long_description=open("README.md").read(),
 )
